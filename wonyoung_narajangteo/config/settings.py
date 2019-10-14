2b3e94eeae36ef44cc46becbe9d15f720b72ebe2
JongMin <standbyme227@algorithmlabs.co.kr>
2019-10-15 00:25:34 +0900

    Wonyoung c12
    supervisord.conf의 path 수정

--- wonyoung_narajangteo/config/settings.py
+++ wonyoung_narajangteo/config/settings.py
@@ -16,8 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+STATIC_DIR = os.path.join(BASE_DIR, '.static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
