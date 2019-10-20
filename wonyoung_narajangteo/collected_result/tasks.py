585ff27f6d4d3872e91b15182bcd2d6d911bcefe
JongMin <standbyme227@algorithmlabs.co.kr>
2019-10-20 16:03:07 +0900

    Wonyoung V2 c8
    금요일에 task가 돌지않아서 수정

--- wonyoung_narajangteo/collected_result/tasks.py
+++ wonyoung_narajangteo/collected_result/tasks.py
@@ -5,7 +5,7 @@ app.conf.beat_schedule = {
     'add-every-10-sec': {
         'task': 'collected_result.tasks.get_announcement',
         # 'schedule': 10.0,
-        'schedule': crontab(hour=14, minute=00, day_of_week="0,1,2,3,4"),
+        'schedule': crontab(hour=16, minute=15, day_of_week="0,1,2,3,4,5"),
     },
 }
 
@@ -36,8 +36,11 @@ def get_announcement():
     now = datetime.today()
 
     if now.weekday() == 0:
-        to_date = f'{now.year}/{now.month}/{now.day}'
-        from_date_data = now - timedelta(3)
+        to_date_data = now - timedelta(2) - timedelta(hours=2) - timedelta(minutes=15)
+        to_date = f'{to_date_data.year}/{to_date_data.month}/{to_date_data.day}'
+        # to_date = f'{now.year}/{now.month}/{now.day}'
+        from_date_data = now - timedelta(3) - timedelta(hours=2) - timedelta(minutes=15)
+        # from_date_data = now - timedelta(3)
         from_date = f'{from_date_data.year}/{from_date_data.month}/{from_date_data.day}'
 
     else:
